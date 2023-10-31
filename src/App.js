35c312917114ba4db6a7fe9d657917251fd1db9d
TI <ti@TIs-MacBook-Pro.local>
2023-10-31 14:53:33 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import './App.css';
+import ButtonTop from './Components/buttonTop';
+import Navbar from "./Components/navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar></Navbar>
+        <Switch>
+          <Route path="/"></Route>
+        </Switch>
+      </Router>
+      <ButtonTop></ButtonTop>
+      <div className="menu">
+        <p>B l u e b i r d</p>
+        <p>[--]</p>
+      </div>
+
+      <div className="blocoHome">
+        <h3> Dublado: incrível <br />como o original</h3>
+        <div className=""></div>
+      </div>
+
+      <div className="blocoContent">
+        <div>
+          <h3 className="titles">Trusted Partner</h3>
+          <p className="line"></p>
+        </div>
+        <div>
+          <img src="#" />
+        </div>
+        <div>
+          <p>Oferecemos serviços em português, inglês e espanhol. Contamos com uma acústica de alta qualidade e engenheiros. Oferecemos serviços em português, inglês e espanhol. Contamos com uma acústica de alta qualidade e engenheiros. Oferecemos serviços em português, inglês e espanhol. Contamos com uma acústica de alta qualidade e engenheiros.
+            Oferecemos serviços em português, inglês e espanhol. Contamos com uma acústica de alta qualidade e engenheiros. Oferecemos serviços em português, inglês e espanhol. Contamos com uma acústica de alta qualidade e.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
+
+
---
+++ src/Components/buttonTop.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+
+function ButtonTop() {
+    const [backToTopButton, setBackToTopButton] = useState(false)
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                setBackToTopButton(true)
+            } else {
+                setBackToTopButton(false)
+            }
+        })
+    }, []) 
+
+    const scrollUp = () => [
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    ]
+
+
+    return <div className='App'>
+
+        {backToTopButton && (
+            <button style={{
+            backgroundColor: "#4F6EDD",
+            width: "4rem",
+            height: "4rem",
+            borderRadius: "20rem",
+            border: "none",
+        
+            position: "fixed",
+            bottom: "1rem",
+            right: "1rem",
+
+            cursor: 'pointer',
+            transition: "ease-in-out",
+            }} onClick={scrollUp}> ddd
+
+            </button>
+
+            
+        )}
+
+        
+
+    </div>
+}
+
+export default ButtonTop;
