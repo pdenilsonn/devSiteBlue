86527c61b1306fc003468adc9e5dc47af8287e73
pdenilsonn <denilso147@hotmail.com>
2024-01-11 10:44:30 -0300

    11 01 2024 1044

--- src/Components/security/Security.js
+++ src/Components/security/Security.js
@@ -9,9 +9,14 @@ function Security() {
                     <p className="line"></p>
                 </div>
                 <div className="blocoContent">
-                   <img alt="TPN logo" src="https://www.ttpn.org/wp-content/themes/mpaa/images/svg/tpn-nav-wtca.png" />
+                    <div>
+                        <img alt="TPN logo" src="https://www.ttpn.org/wp-content/themes/mpaa/images/svg/tpn-nav-wtca.png" />
+                    </div>
+                    <div>
+                    <h3 className='titleSecurity'>Todo seu <span> Conteúdo Protegido</span> do início ao fim.</h3>
                     <p>
                         Levamos a proteção dos conteúdos ao mais alto nível e somos orgulhosos parceiros da Trusted Partner Network (TPN). O protocolo, baseado nas melhores práticas de segurança, nos permite manter seus conteúdos protegidos. Com a aplicação dos procedimentos e recomendações da TPN, garantimos serviços de alta qualidade mantendo a segurança física e digital em todas as etapas.</p>
+                    </div>
                 </div>
             </div>
         </>
--- src/Components/services/Services.js
+++ src/Components/services/Services.js
@@ -12,8 +12,8 @@ function Services() {
         <div className="blocoContentS">
           <div className="serviceCard">
             <div className='cardTitle'>
-            <span><i class="fa-solid fa-microphone-lines" /></span>
-            <h3>Gravando</h3>
+              <span><i class="fa-solid fa-microphone-lines" /></span>
+              <h3>Gravando</h3>
             </div>
             <p>Estrutura para atender demandas de produção urgentes com eficiência e rapidez. Capacidade para mais de 200 horas de gravação mensal.
             </p>
@@ -21,8 +21,8 @@ function Services() {
 
           <div className="serviceCard">
             <div className='cardTitle'>
-            <span><i class="fa-solid fa-sliders" /></span>
-            <h3>Como No Original</h3>
+              <span><i class="fa-solid fa-sliders" /></span>
+              <h3>Como No Original</h3>
             </div>
             <p>Estúdios projetados com excelência acústica, utilizando tecnologia de ponta e os melhores profissionais do mercado.
             </p>
@@ -30,18 +30,18 @@ function Services() {
 
           <div className="serviceCard">
             <div className='cardTitle'>
-            <span><i class="fa-solid fa-shield-halved" /></span>
-            <h3>100% Seguro</h3>
+              <span><i class="fa-solid fa-shield-halved" /></span>
+              <h3>100% Seguro</h3>
             </div>
             <p>Rede interna protegida contra ataques cibernéticos e vazamento de dados, do início ao fim de todos os nossos projetos.</p>
           </div>
 
           <div className="serviceCard">
             <div className='cardTitle'>
-            <span><i class="fa-solid fa-shield-halved" /></span>
-            <h3>100% Seguro</h3>
+              <span><i class="fa-solid fa-clock"/></span>
+              <h3>Em Tempo Real</h3>
             </div>
-            <p>Rede interna protegida contra ataques cibernéticos e vazamento de dados, do início ao fim de todos os nossos projetos.</p>
+            <p>Nossa equipe oferece informações em tempo real sobre o progresso do seu projeto. Oferecemos relatórios diários e semanais.</p>
           </div>
 
         </div>
